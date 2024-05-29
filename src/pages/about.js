f1dac8577502a8ba2bd9f0f1c078bea5dd2d8d2a
eqsquared <jasonpcarroll@gmail.com>
2024-05-29 17:18:27 -0500

    next project

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>About Us - EQ²</title>
+            </Head>
+            <h1>About Us</h1>
+            <p>This is the about page.</p>
+        </div>
+    )
+}
