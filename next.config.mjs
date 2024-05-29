a5f1727fa931fdbdf6a4a7245bd91856acdf4d76
eqsquared <jasonpcarroll@gmail.com>
2024-05-29 17:58:33 -0500

    next project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     reactStrictMode: true,
+    // Adjust this to the name of your GitHub repository
+    basePath: isProd ? '/eq2-next/' : '',
+    assetPrefix: isProd ? '/eq2-next/' : '',
+    images: {
+        loader: 'default',
+        path: isProd ? '/eq2-next/' : '',
+    },
 };
 
 export default nextConfig;
